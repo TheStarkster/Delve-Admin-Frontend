d885a3db24e2ff029bc5211553f07aa7d74f48a1
Gurkaran Singh <sgurkaran2000@gmail.com>
2020-04-26 23:34:57 +0530

    design ready

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import Notifications from "views/Notifications.js";
 import TableList from "views/TableList.js";
 import Typography from "views/Typography.js";
 import UserProfile from "views/UserProfile.js";
+import ManageEvents from "views/ManageEvents.js";
 
 var routes = [
   {
@@ -23,22 +24,14 @@ var routes = [
   //   component: Icons,
   //   layout: "/admin",
   // },
-  // {
-  //   path: "/map",
-  //   name: "Map",
-  //   rtlName: "خرائط",
-  //   icon: "tim-icons icon-pin",
-  //   component: Map,
-  //   layout: "/admin",
-  // },
-  // {
-  //   path: "/notifications",
-  //   name: "Notifications",
-  //   rtlName: "إخطارات",
-  //   icon: "tim-icons icon-bell-55",
-  //   component: Notifications,
-  //   layout: "/admin",
-  // },
+  {
+    path: "/manage-events",
+    name: "Manage Events",
+    rtlName: "إخطارات",
+    icon: "tim-icons icon-calendar-60",
+    component: ManageEvents,
+    layout: "/admin",
+  },
   {
     path: "/user-profile",
     name: "Manage Users",
@@ -47,21 +40,5 @@ var routes = [
     component: UserProfile,
     layout: "/admin",
   },
-  // {
-  //   path: "/tables",
-  //   name: "Table List",
-  //   rtlName: "قائمة الجدول",
-  //   icon: "tim-icons icon-puzzle-10",
-  //   component: TableList,
-  //   layout: "/admin",
-  // },
-  // {
-  //   path: "/typography",
-  //   name: "Typography",
-  //   rtlName: "طباعة",
-  //   icon: "tim-icons icon-align-center",
-  //   component: Typography,
-  //   layout: "/admin",
-  // },
 ];
 export default routes;
